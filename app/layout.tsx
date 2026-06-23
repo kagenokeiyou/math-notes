b736e28c52799df0cf6182f42084a14796f9d67c
kagenokeiyou <kagenokeiyou@outlook.com>
2026-06-23 23:18:01 +0800

    Config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,21 @@
-import { Inter } from 'next/font/google';
-import { Provider } from '@/components/provider';
-import './global.css';
+import { Inter } from "next/font/google";
+import { Provider } from "@/components/provider";
+import "./global.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(
+    process.env.NODE_ENV == "production"
+      ? "https://kagenokeiyou.github.io/math-notes"
+      : "http://localhost:3000",
+  ),
+};
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
-export default function Layout({ children }: LayoutProps<'/'>) {
+export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
