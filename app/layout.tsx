6332649fd545dfad9007a3c7fe2ebb97c15ab5e2
kagenokeiyou <kagenokeiyou@outlook.com>
2026-06-23 23:25:11 +0800

    Config and format

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,25 @@
-import { Inter } from "next/font/google";
-import { Provider } from "@/components/provider";
-import "./global.css";
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './global.css';
+import { Provider } from '@/components/provider';
+import 'katex/dist/katex.css';
 
 export const metadata: Metadata = {
   metadataBase: new URL(
-    process.env.NODE_ENV == "production"
-      ? "https://kagenokeiyou.github.io/math-notes"
-      : "http://localhost:3000",
+    process.env.NODE_ENV == 'production'
+      ? 'https://kagenokeiyou.github.io/math-notes'
+      : 'http://localhost:3000',
   ),
 };
 
 const inter = Inter({
-  subsets: ["latin"],
+  subsets: ['latin'],
 });
 
-export default function Layout({ children }: LayoutProps<"/">) {
+export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
+      <body className="flex min-h-screen flex-col">
         <Provider>{children}</Provider>
       </body>
     </html>
