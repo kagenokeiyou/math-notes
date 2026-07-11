8375b9de8d0f383e4690a021645bab24c16ea5e8
kagenokeiyou <kagenokeiyou@outlook.com>
2026-07-11 18:23:18 +0800

    Update home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,15 +1,16 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { buttonVariants } from '@/components/ui/button'
+import { appName } from '@/lib/shared'
 import favicon from '@/public/favicon.png'
 
 export default function HomePage() {
   return (
-    <div className="flex flex-1 flex-col items-center gap-4 pt-[10vh]">
+    <div className="flex flex-1 flex-col items-center gap-4 px-[10vw] py-[10vh] text-center">
       <Image src={favicon} alt="favicon" width={128} height={128} loading="eager" />
-      <h1 className="text-center text-4xl font-bold">Math Notes</h1>
-      <h2 className="text-fd-muted-foreground text-center text-2xl font-bold">My Math Notes</h2>
-      <div className="flex flex-wrap justify-center gap-4">
+      <h1 className="text-4xl font-bold">{appName}</h1>
+      <h2 className="text-fd-muted-foreground text-2xl font-bold">My {appName}</h2>
+      <div className="flex flex-col items-center gap-4">
         <Link href="/docs/basics" className={buttonVariants({ variant: 'secondary' })}>
           Basics
         </Link>
