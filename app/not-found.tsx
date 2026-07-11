29fae8e20f829a9e3fb8a27566c04d6352939e3c
kagenokeiyou <kagenokeiyou@outlook.com>
2026-07-11 18:24:10 +0800

    Add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import { buttonVariants } from '@/components/ui/button'
+
+export default function NotFound() {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-2 text-center">
+      <h1 className="text-4xl font-medium">404</h1>
+      <h2 className="text-lg font-normal">This page could not be found</h2>
+      <Link href="/" className={buttonVariants({ variant: 'secondary' })}>
+        Back to home
+      </Link>
+    </div>
+  )
+}
